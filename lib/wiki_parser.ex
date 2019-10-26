bc10e8d609bfbb27fd788f7c171dc22c4d89baa4
Charles Watson <sircharleswatson@gmail.com>
2019-10-25 20:40:05 -0500

    connect to wikipedia API to search for games

--- lib/wiki_parser.ex
+++ lib/wiki_parser.ex
@@ -2,17 +2,54 @@ defmodule WikiParser do
   @moduledoc """
   Documentation for WikiParser.
   """
+  use Tesla
 
-  @doc """
-  Hello world.
+  plug Tesla.Middleware.BaseUrl, "https://en.wikipedia.org"
+  plug Tesla.Middleware.JSON
 
-  ## Examples
+  def search_for_game(game) do
+    game =
+      String.downcase(game)
 
-      iex> WikiParser.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    result =
+      get("/w/api.php?action=query&list=search&utf8=&format=json&srprop=snippet&srsearch=#{URI.encode(game)}%20video%20game")
+      |> get_search_results()
+      |> filter_search_results(game)
   end
+
+  def get_search_results({:ok, response}) do
+    response
+    |> Map.get(:body)
+    |> Map.get("query")
+    |> Map.get("search")
+  end
+
+  def filter_search_results(search_results, game) do
+    filtered_results =
+      search_results
+      |> Enum.reject(fn result ->
+        title = String.downcase(result["title"])
+
+        !String.contains?(title, game)
+      end)
+      |> Enum.reject(fn result ->
+        title = String.downcase(result["title"])
+
+        if title != game do
+          !String.contains?(title, "video game")
+        else
+          false
+        end
+      end)
+
+    if length(filtered_results) > 1 do
+      Enum.reject(filtered_results, fn result ->
+        !String.contains?(result["title"], "video game")
+      end)
+    else
+      filtered_results
+    end
+  end
+
+
 end
--- mix.exs
+++ mix.exs
@@ -23,6 +23,10 @@ defmodule WikiParser.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:hackney, "~> 1.14.0"},
+      {:html_sanitize_ex, "~> 1.3"},
+      {:jason, ">= 1.0.0"},
+      {:tesla, "~> 1.2.1"}
     ]
   end
 end
